6e57cf1318177674e88b65ee0aaf21a997534fc1
pawel <paw.zeller@gmail.com>
2018-04-18 22:26:25 +0200

    Git upload

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import PostfixCalc
 import Function
+import sys
 
 
 def simplyfy(input, debug):
@@ -23,28 +24,9 @@ def simplyfy(input, debug):
 
 if __name__ == "__main__":
 
-   # simplyfy()
-    fun = 'a | b > c'
+    if len(sys.argv)>1:
+       fun = sys.argv[1]
+    else:
+        fun = 'a | b > c'
+
     simplyfy(fun,True)
-    #fun = 'a | a v (a > b) = (c > d)'
-    #fun = '((a |      b) & ad )> c'
-    #fun = '( ! abba  ) | ( b > c > d     )'
-    #print("Input: " + fun)
-
-    #f = Function.Function(fun)
-    #print(f.function)
-    #print(f.variables)
-
-    #p = PostfixCalc.PostfixCalc(f)
-    #print("Postfix:")
-    #print(p.postfix)
-    #print("Variables:")
-    #print(p.variables)
-
-    #print("Possibilities:")
-    #print(p.values())
-    #p.reductor()
-    #f.reducted = p.reductor()
-
-    #print("Evaluation: ")
-    #print(p)
